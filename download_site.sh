030b8d34484fc62d56576b7022f3b6b5a9c1025d
Derek Driggs <dtd24@cantab.ac.uk>
2024-01-10 23:07:53 +0000

    remove marketing banner

--- download_site.sh
+++ download_site.sh
@@ -2,8 +2,5 @@ wget -k -K  -E -r -l 10 -p -N -F --restrict-file-names=windows -nH testdomain845
 
 BAD_BANNER="<link crossorigin='anonymous' rel='stylesheet' id='all-css-2-1' href='https://s2.wp.com/_static/??/wp-content/blog-plugins/wordads/global.css,/wp-content/blog-plugins/marketing-bar/css/marketing-bar.css?m=1697652279j&cssminify=yes' type='text/css' media='all' />"
 
-find . -mindepth 1 -type f -exec sed -i '' "s/${BAD_BANNER}//g" {} \;
-
-
-find . -mindepth 1 -type f -exec sed -i "" "s/${BAD_BANNER}//g" {} \;
+find . -mindepth 1 -type f -exec sed -i '' "s|${BAD_BANNER}||g" {} \;
 
